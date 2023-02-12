b63dfeb1e80535dbf5dd8a6f21748fb04b41140b
abdourahamaneabdoulaye36@gmail.com <abdourahamaneabdoulaye36@gmail.com>
2023-02-12 23:49:47 +0100

    commit0

---
+++ numberToString.js
@@ -0,0 +1,7 @@
+function numberToString(arg) {
+  var  Entrer =  new  Array(1,2,3,4,5,6,7,8,9,10,);
+  console.log(Entrer);
+  var Sortie = new Array("un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix");
+  console.log(Sortie);
+}
+module.exports = numberToString();
